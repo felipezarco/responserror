0cef0e03b1ff71462eac99a9f5e08f0ec091fe23
felipezarco <felipezarco@hotmail.com>
2022-08-25 21:24:05 -0300

    turn Responserror into a class

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
 import { Request, Response, NextFunction } from 'express'
 import HttpStatus from 'http-status-codes'
 
+
 const camelCase = (str: string) => str.toLowerCase().replace(/(\_\w)/g, c => c[1].toUpperCase())
 
-const errorHandler = (error: any, request: Request, response: Response, next: NextFunction) => {
+type IOptions = {
+  promptErrors: boolean | (() => boolean) 
+}
+
+type IResponserrorObject = { 
+  code: number
+  status: string
+  message: string
+  success: boolean
+  errors: any
+}
+
+class Responserror {
   
-  /* default */
-  const responseObject = {
+  private options: IOptions
+  
+  private mapStatusByCode: { [code: number]: string } = {}
+  
+  private responserror: IResponserrorObject = {
     code: 500,
     status: 'INTERNAL_SERVER_ERROR',
     message: 'Internal Server Error',
@@ -14,84 +30,74 @@ const errorHandler = (error: any, request: Request, response: Response, next: Ne
     errors: undefined
   }
   
-  // if(error == {}) return response.send(responseObject.code).json(responseObject)
-  
-  const mapObjectMethods: { [code: number]: string } = {}
-
-  for(const [httpStatus, httpCode] of Object.entries(HttpStatus)) {
-    if(!httpStatus.startsWith('get') && typeof httpCode !== 'function' && !['1','2'].includes(String(httpCode).charAt(0))) {
-      Object.assign(mapObjectMethods, { [httpCode]: camelCase(httpStatus) })
+  private setMapStatusByCode = () => {
+    for(const [httpStatus, httpCode] of Object.entries(HttpStatus)) {
+      if(!httpStatus.startsWith('get') && typeof httpCode !== 'function' && !['1','2'].includes(String(httpCode).charAt(0))) {
+        Object.assign(this.mapStatusByCode, { [httpCode]: camelCase(httpStatus) })
+      }
     }
   }
   
-  const automaticMethod: { code: number, status: string } | null = ((error) => {
-    
-    if(!error) return null
-    
-    /* Code was given */
-    if(error.code && Object.keys(mapObjectMethods).includes(String(error.code))) {
-      for(const [httpStatus, httpCode] of Object.entries(HttpStatus)) {
-        if(!httpStatus.startsWith('get') && typeof httpCode !== 'function' && !['1','2'].includes(String(httpCode).charAt(0))) {
-          if(httpCode == error.code) {
-            return { 
-              code: httpCode, 
-              status: httpStatus
-            } 
-          }
-        }
-      }
-    } 
-    
-    /* Status was given */
-    if(error.status) {
-      for(const [httpStatus, httpCode] of Object.entries(HttpStatus)) {
-        if(!httpStatus.startsWith('get') && typeof httpCode !== 'function' && !['1','2'].includes(String(httpCode).charAt(0))) {
-          if(String(httpStatus).trim().toUpperCase() == String(error.status).trim().toUpperCase()) {
-            return { 
-              code: httpCode, 
-              status: httpStatus
-            } 
-          }
-        }
-      }
+  private setDefaultValuesForResponserror = () => {
+    if(!this.responserror.code && !this.responserror.status && !this.responserror.message) {
+      this.responserror.code = 500
+      this.responserror.status = 'INTERNAL_SERVER_ERROR',
+      this.responserror.message = 'Internal Server Error',
     }
-    
-    return null
-  })(error)
+    if(!this.responserror.success) this.responserror.success = false,
+    if(!this.responserror.errors) this.responserror.errors = undefined
+  }
   
-  const getStatusText = function(code: string | number) {
-    try {
+  public getStatusTextByCode = function(code: string | number) {
+    try { 
       return HttpStatus.getStatusText(code)
     } catch(e) {
       return undefined
     }
   }
+   
+  public getStatusByCode = (code: number): string | undefined => this.mapStatusByCode[code]
   
-  const statusText: string | undefined | null = automaticMethod ? getStatusText(automaticMethod.code) : null
-
-  /* inferred */
-  if(automaticMethod) {
-    responseObject.code = automaticMethod?.code,
-    responseObject.status = automaticMethod?.status
+  public getCodeByStatus = (status: string): number | undefined => {
+    for(const [httpStatus, httpCode] of Object.entries(HttpStatus)) {
+      if(!httpStatus.startsWith('get') && typeof httpCode !== 'function' && !['1','2'].includes(String(httpCode).charAt(0))) {
+        if(String(httpStatus).trim().toUpperCase() == String(status).trim().toUpperCase()) {
+          return httpCode
+        }
+      }
+    }
   }
   
-  if(statusText) responseObject.message = statusText
+  constructor(options: IOptions) {
+    this.options = options
+    this.setMapStatusByCode()
+  }
   
-  // if(error?.code) responseObject.code = error.code
-  // if(error?.status) responseObject.status = error.status
-  // if(error?.message) responseObject.message = error.message
-  // if(error?.errors) responseObject.errors = error.errors
-  // if(error?.success) responseObject.success = error.success
+  errorHandler = (error: any, request: Request, response: Response, next: NextFunction) => {
+    
+    if(this.options.promptErrors === true || (typeof this.options.promptErrors === 'function' && this.options.promptErrors())) {
+      
+    }
+    
+    const statusText: string | undefined = this.getStatusTextByCode(this.responserror.code)
+    
+    this.responserror.message = error.message 
+    
+    // @ts-ignore
+    if(automaticMethod?.status && typeof response[`send_${automaticMethod.status}`] === 'function') {
+      // @ts-ignore
+      return response[`send_${automaticMethod}`](
+        error?.message ?? statusText, error?.errors ?? error?.content
+      )
+    }
+    
+    this.setDefaultValuesForResponserror()
+     
+    return response.status(this.responserror.code).json({ ...this.responserror, ...error })
+  }
   
-  // @ts-ignore
-  // if(automaticMethod?.status && typeof response[`send_${automaticMethod.status}`] === 'function') {
-  //   // @ts-ignore
-  //   return response[`send_${automaticMethod}`](
-  //     error?.message ?? statusText, error?.errors ?? error?.content
-  //   )
-  // }
   
-  return response.status(responseObject.code).json({ ...responseObject, ...error })
+  
 }
- 
-export default errorHandler
+
+export default Responserror
